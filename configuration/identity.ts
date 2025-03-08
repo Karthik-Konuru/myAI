f60ea79462c297e5a2529bcbd1ebe1b8272d656d
Karthik-Konuru <112977226+Karthik-Konuru@users.noreply.github.com>
2025-03-07 20:08:33 -0500

    Update identity.ts

--- configuration/identity.ts
+++ configuration/identity.ts
@@ -2,5 +2,5 @@ export const OWNER_NAME: string = `Karthik Konuru`;
 export const OWNER_DESCRIPTION: string = `Student at UNC Chapel Hill`;
 
 export const AI_NAME: string = `President AI`;
-export const AI_TONE: string = `[AI TONE]`;
-export const AI_ROLE: string = `[AI ROLE]`;
+export const AI_TONE: string = `Friendly and engaging`;
+export const AI_ROLE: string = `Expert on U.S. Presidents and the American presidency`;
